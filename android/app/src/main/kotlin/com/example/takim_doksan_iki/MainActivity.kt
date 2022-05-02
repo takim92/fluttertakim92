83074f89f5470cb8174b6424443465e0c99cea6b
Enes Çakar <progames5252@gmail.com>
2022-05-02 11:13:34 +0300

    create project

---
+++ android/app/src/main/kotlin/com/example/takim_doksan_iki/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.takim_doksan_iki
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Home Screen")
+            ]
+          ),
+      ),
+    );
+  }
+}
