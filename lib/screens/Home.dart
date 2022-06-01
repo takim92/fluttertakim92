29136119b407c03353cbac7f3fcf254d85b7b1e8
Enes Çakar <progames5252@gmail.com>
2022-06-01 11:33:51 +0300

    edit home page

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -205,11 +205,14 @@ class _HomeState extends State<Home> {
       padding: const EdgeInsets.all(8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Doktor Notu:", style: TextStyle(color: Colors.red, fontWeight: FontWeight.w500, fontSize: 16),),
+        children: const [
           Text(
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
-          )
+            "Doktor Notu:",
+            style: TextStyle(
+                color: Colors.red, fontWeight: FontWeight.w500, fontSize: 16),
+          ),
+          Text(
+              "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).")
         ],
       ),
     );
