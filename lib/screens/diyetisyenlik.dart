d926287912ebabda5ad01d91dd18e856599cd2db
feyzaugz <93938663+feyzaugz@users.noreply.github.com>
2022-06-06 08:09:19 +0300

    Update diyetisyenlik.dart

--- lib/screens/diyetisyenlik.dart
+++ lib/screens/diyetisyenlik.dart
@@ -1,12 +1,156 @@
 import 'package:flutter/material.dart';
 
-class Diyetisyenlik extends StatelessWidget {
-  const Diyetisyenlik({ Key? key }) : super(key: key);
+void main() {
+  runApp(const diyetisyen_sekmesi());
+}
+class diyetisyen_sekmesi extends StatefulWidget {
+  const diyetisyen_sekmesi({Key? key}) : super(key: key);
 
   @override
+  _diyetisyen_sekmesiState createState() => _diyetisyen_sekmesiState();
+}
+
+class _diyetisyen_sekmesiState extends State<diyetisyen_sekmesi> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          title: Center(
+            child: Text(
+              'Diyetisyenlik Sekmesi',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.home,
+              ),
+              onPressed: (){
+              },
+            )
+          ],
+          leading: Builder(
+              builder: (BuildContext context){
+                return IconButton(onPressed: (){
+                  Navigator.pop(context);
+                }, icon: Icon(Icons.keyboard_arrow_left),
+                );
+              }
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 25.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.network(
+                  'https://www.lidertipmerkezi.com.tr/wp-content/uploads/2020/11/DIYETISYEN-HIZMETI.jpg',
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Card(
+                  elevation: 2,
+                  margin: EdgeInsets.symmetric(horizontal: 60.0),
+                  color: Color(0xfffce4ec),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      30,
+                    ),
+                  ),
+                  child: ListTile(
+                    title: Center(
+                      child: Text(
+                        'Diyetisyenler',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 19,
+                          fontStyle:FontStyle.italic,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    onTap: (){
+
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Card(
+                  elevation: 2,
+                  margin: EdgeInsets.symmetric(horizontal: 60.0),
+                  color: Color(0xfffce4ec),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      30,
+                    ),
+                  ),
+                  child: ListTile(
+                    title: Center(
+                      child: Text(
+                        'Gıda/Kalori Takibi',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 19,
+                          fontStyle:FontStyle.italic,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    onTap: (){
+                      //Navigator.push(context, new MaterialPageRoute(builder: context)) =>
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Card(
+                  elevation: 2,
+                  margin: EdgeInsets.symmetric(horizontal: 60.0),
+                  color: Color(0xfffce4ec),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      30,
+                    ),
+                  ),
+                  child: ListTile(
+                    title: Center(
+                      child: Text(
+                        'Yakılan Enerji',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 19,
+                          fontStyle:FontStyle.italic,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    onTap: (){
+
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
