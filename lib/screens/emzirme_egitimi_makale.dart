682eae1eaf93036844b887c3be1fc6f01fe7cca1
feyzaugz <93938663+feyzaugz@users.noreply.github.com>
2022-06-06 08:18:18 +0300

    Create emzirme_egitimi_makale.dart

---
+++ lib/screens/emzirme_egitimi_makale.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const yeni_anne());
+}
+class yeni_anne extends StatefulWidget {
+  const yeni_anne({Key? key}) : super(key: key);
+
+  @override
+  State<yeni_anne> createState() => _yeni_anneState();
+}
+
+class _yeni_anneState extends State<yeni_anne> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          title: Center(
+            child: Text(
+              'Emzirme Eğitimi',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.home,
+              ),
+              onPressed: (){
+              },
+            )
+          ],
+          leading: Builder(
+              builder: (BuildContext context){
+                return IconButton(onPressed: (){
+                  Navigator.pop(context);
+                }, icon: Icon(Icons.keyboard_arrow_left),
+                );
+              }
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 25.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.network(
+                    'https://www.koruhastanesi.com/images/haber/main/522021151938505.jpg',
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'HER ÖĞÜNDE TEK MEME',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.brown,
+                    height: 30,
+                  ),
+                  Text(
+                      'Bebek her beslenmede bir memedeki sütü tamamen almalıdır. Emzirmenin sonlarına doğru yağdan ve kaloriden zengin sütü alarak bebek doyacaktır. Bu nedenle her öğünde bir göğsü bitirmek oldukça önemlidir.'
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Bebeğinizi emzirmenin en sağlıklı yollarını biliyor musunuz?',
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                      'Anne rahat bir pozisyonda oturmalı, sırt ve bel rahat bir yastıkla desteklenmelidir. Anne, bebeği emzireceği kolunun altına bir yastık alarak emzirme süresince kolunu havada tutmaktan kaçınmalıdır.Bebek emzirilirken mümkün oldukça anne kendine yakın tutmalıdır. Bebek kolun iç yüzeyine, baş dirsek boşluğuna gelecek şekilde yerleştirilmelidir. Ardından sırtı boyunca popoya kadar el ile desteklenmelidir.Bebek vücut olarak tamamen anneye dönük, karın karına olmalıdır. Bebeğin emzirme esnasında baş ve vücudunun aynı düzlemde olmasına dikkat edilmelidir.'
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                      'Bebek emerken dudakları dışarıya dönük olmalıdır. Bebek memenin kahverengi kısmının hepsini mümkün olduğunca ağzına almalıdır.Emerken yutkunma sesi haricinde bir ses çıkıyorsa, yanaklarında çukur oluşuyorsa, çenesi memeye değmiyorsa yanlış emdiğinin belirtisidir.'
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                      'Bebek sadece meme ucunu emerse, meme ucunu sert damağıyla ezeceği için anne memesinde çatlaklara sebep olabilir. Bu durumda, bebek hem sütü boşaltamayacak hem de anne için ağrılı bir emzirmeye neden olacaktır.Anne emzirme öncesi memesinde dolgunluk ve sertlik olup olmadığını kontrol etmelidir. Eğer ele sert alanlar gelirse sıcak uygulama yapılabilir. Örneğin; duşa girilebilir, havlu ısıtılıp konulabilir, küçük sıcak su torbası ince bir havluya sarılarak sıcak uygulama yapılabilir.'
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
