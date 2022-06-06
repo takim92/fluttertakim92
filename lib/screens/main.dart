1ccf252655b973df32bb95b891126e877757f32c
feyzaugz <93938663+feyzaugz@users.noreply.github.com>
2022-06-06 17:15:08 +0300

    Create main.dart

---
+++ lib/screens/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:proje_yeni_anne/yeni_anne.dart';
+
+
+void main() {
+  runApp(const MyApp()
+  );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Uygulama İsmi',
+      theme: ThemeData(
+        primaryColor: Colors. blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const YeniAnnePage(),
+    );
+  }
+}
